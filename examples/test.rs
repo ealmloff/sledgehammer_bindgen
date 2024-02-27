850408c054a07af3f035f925df7850f529928942
Evan Almloff <ealmlof1@stumail.jccc.edu>
2024-02-27 14:16:53 -0600

    Update test.rs

--- examples/test.rs
+++ examples/test.rs
@@ -10,7 +10,7 @@ fn main() {
         const JS: &str = r#"this.nodes = [document.getElementById("main")];"#;
 
         fn create_element(id: u16, name: &'static str<u8, name_cache>) {
-            "nodes[$id$]=document.createElement($name$);"
+            "this.nodes[$id$]=document.createElement($name$);"
         }
 
         fn create_element_ns(
@@ -18,47 +18,47 @@ fn main() {
             name: &'static str<u8, name_cache>,
             ns: &'static str<u8, ns_cache>,
         ) {
-            "nodes[$id$]=document.createElementNS($ns$,$name$);"
+            "this.nodes[$id$]=document.createElementNS($ns$,$name$);"
         }
 
         fn set_attribute(id: u16, name: &'static str<u8, name_cache>, val: impl Writable<u8>) {
-            "nodes[$id$].setAttribute($name$,$val$);"
+            "this.nodes[$id$].setAttribute($name$,$val$);"
         }
 
         fn remove_attribute(id: u16, name: &'static str<u8, name_cache>) {
-            "nodes[$id$].removeAttribute($name$);"
+            "this.nodes[$id$].removeAttribute($name$);"
         }
 
         fn append_child(id: u16, id2: u16) {
-            "nodes[$id$].appendChild(nodes[$id2$]);"
+            "this.nodes[$id$].appendChild(nodes[$id2$]);"
         }
 
         fn insert_before(parent: u16, id: u16, id2: u16) {
-            "nodes[$parent$].insertBefore(nodes[$id$],nodes[$id2$]);"
+            "this.nodes[$parent$].insertBefore(nodes[$id$],nodes[$id2$]);"
         }
 
         fn set_text(id: u16, text: impl Writable<u8>) {
-            "nodes[$id$].textContent=$text$;"
+            "this.nodes[$id$].textContent=$text$;"
         }
 
         fn remove(id: u16) {
-            "nodes[$id$].remove();"
+            "this.nodes[$id$].remove();"
         }
 
         fn replace(id: u16, id2: u16) {
-            "nodes[$id$].replaceWith(nodes[$id2$]);"
+            "this.nodes[$id$].replaceWith(this.nodes[$id2$]);"
         }
 
         fn clone(id: u16, id2: u16) {
-            "nodes[$id2$]=nodes[$id$].cloneNode(true);"
+            "this.nodes[$id2$]=this.nodes[$id$].cloneNode(true);"
         }
 
         fn first_child(id: u16) {
-            "node[id]=node[id].firstChild;"
+            "this.node[id]=this.node[id].firstChild;"
         }
 
         fn next_sibling(id: u16) {
-            "node[id]=node[id].nextSibling;"
+            "this.node[id]=this.node[id].nextSibling;"
         }
     }
 
